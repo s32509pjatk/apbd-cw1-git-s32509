67f38dfdcedcd0f5d2cde7de64e4fa7ef8ee6c28
Martyna Dang Van <s32509@pjwstk.edu.pl>
2026-03-12 20:35:05 +0100

    Add length check for inputs

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main {
         if (x == null || y == null) {
             throw new IllegalArgumentException("punkty sa nullami");
         }
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("punkty nie maja tych samych wymiarów");
+
+        }
         for (int i = 0; i < x.length; i++) {
             double wzor = Math.pow(x[i] - y[i], 2);
             wynik = wynik + wzor;
