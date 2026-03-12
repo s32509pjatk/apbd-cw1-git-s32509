e1da3c554ea91e467b20b72d671cdd6273eea34c
Martyna Dang Van <s32509@pjwstk.edu.pl>
2026-03-12 20:28:23 +0100

    Add null check for input in the odlegloscXY method

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static double odlegloscXY(double[] x, double[] y){
         double wynik = 0;
+        if (x == null || y == null) {
+            throw new IllegalArgumentException("punkty sa nullami");
+        }
         for (int i = 0; i < x.length; i++) {
             double wzor = Math.pow(x[i] - y[i], 2);
             wynik = wynik + wzor;
